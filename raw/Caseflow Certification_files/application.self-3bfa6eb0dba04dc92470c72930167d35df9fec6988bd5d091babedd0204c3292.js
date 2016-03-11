06485b5a73b9b4f14a641c11274f74871cf05a57
Pete Hodgson <git@thepete.net>
2016-03-10 16:11:49 -0800

    add raw page saved directly from Rails app

---
+++ raw/Caseflow Certification_files/application.self-3bfa6eb0dba04dc92470c72930167d35df9fec6988bd5d091babedd0204c3292.js
@@ -0,0 +1,37 @@
+/* globals Clipboard */
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+
+
+
+
+$(function() {
+  window.Dropdown.bind();
+  window.Modal.bind();
+  window.LoadingIndicator.bind();
+});
+
+/* Copies appeals ID to clipboard */
+$(function () {
+  "use strict";
+  new Clipboard('[data-clipboard-text]');
+});
+
+/* Reusable 'refresh' pattern */
+$(function() {
+  $('.cf-action-refresh').on('click', function() {
+    location.reload(); return false;
+  });
+});
